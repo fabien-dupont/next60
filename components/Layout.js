e54bf2057eeed37d66ac36e0b469fb5c682e86da
Fabien Dupont <fab.d1987@yandex.com>
2020-03-10 20:37:49 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,15 @@
 import Header from './Header';
+import Footer from './Footer';
 const Layout = (props) => (
   <div>
     <Header/>
-    <div className="container ">
-      <section className="section">
-        <div className="h-100 d-flex justify-content-center align-items-center">
-          {props.children}
-        </div>
-      </section>
-    </div>
+    <section className="section">
+      <div className="container  h-100  justify-content-center align-items-center">
+        {props.children}
+      </div>
+    </section>
+    <Footer/>
+
   </div>
 );
 export default Layout;
