44692068c82ca535e8f7f5e29dfe6aa9a06bd307
Fabien Dupont <fab.d1987@yandex.com>
2020-03-10 20:44:34 +0100

    insert sectionStyle

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,15 @@
 import Header from './Header';
 import Footer from './Footer';
+const sectionStyle = {minHeight:"96vh"};
 const Layout = (props) => (
   <div>
     <Header/>
-    <section className="section">
-      <div className="container  h-100  justify-content-center align-items-center">
+    <section  style={sectionStyle} className="section w-100 h-100">
+      <div className="container-fluid p-4 ">
         {props.children}
       </div>
     </section>
     <Footer/>
-
   </div>
 );
 export default Layout;
