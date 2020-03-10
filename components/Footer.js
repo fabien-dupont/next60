1843b02883edf2f5f3c8406b2162c1650a6287b3
Fabien Dupont <fab.d1987@yandex.com>
2020-03-10 20:37:53 +0100

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import Link from "next/link" ;
+
+const Footer = () => (
+  <footer className="footer pt-4 mt-4   grey lighten-3 text-center text-md-left">
+    <div className="container-fluid">
+        &copy; 2020 Copyright: fabien dupont
+     </div>
+  </footer>
+);
+
+export default Footer;
