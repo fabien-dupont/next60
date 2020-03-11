184a086b2d2fe2e16f2fa6de52ee3fb1378887eb
Fabien Dupont <fab.d1987@yandex.com>
2020-03-11 17:01:18 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import Header from './Header';
 import Footer from './Footer';
+
 const sectionStyle = {minHeight:"96vh"};
 const Layout = (props) => (
   <div>
@@ -7,9 +8,10 @@ const Layout = (props) => (
     <section  style={sectionStyle} className="section w-100 h-100">
       <div className="container-fluid p-4 ">
         {props.children}
-      </div>
+      </div>    
     </section>
     <Footer/>
   </div>
 );
+
 export default Layout;
