d89f01b01d39d00bf2d649eb902525e5fd481211
Fabien Dupont <fab.d1987@yandex.com>
2020-03-12 16:51:38 +0100

    Create Navbar.js

---
+++ components/header/Navbar.js
@@ -0,0 +1,25 @@
+import Link from "next/link" ;
+import Navheader from "./Navheader" ;
+const Navbar = (props) => {
+  return (
+  <nav className="navbar navbar-expand-lg ">
+    <div className="container-fluid">
+      <Navheader/>
+      
+      <ul className="nav navbar-nav flex-row">
+        <li className="m-auto p-2">
+          <Link href="/"><a>Home</a></Link>
+        </li>
+        <li className="m-auto p-2">
+          <Link href="about"><a>About</a></Link>
+        </li>
+        <li className="m-auto p-2">
+          <Link href="contact"><a>Contact</a></Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  );
+};
+
+export default Navbar;
