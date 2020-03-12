f99d30b7a39c222ddb603987298691b90fcfd015
Fabien Dupont <fab.d1987@yandex.com>
2020-03-12 10:03:43 +0100

    add some flex and jsx css

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,35 @@ const introStyle = {fontSize: 32};
 const Index = () => (
   <Layout>
     <div className="container-fluid" >
-      <h1 className="text-uppercase">{attributes.title}</h1>
-      <br/>
-      <div className="mt-2 mb-2" id="zone_api_result" >
-        <img width="100%" src={imgURL}/>
+      <div className = "row flex-row ">
+        <div id="zone_api_result"  className="p-4 ml-auto mr-0">
+           
+            <img src={imgURL} height={320} /> 
+        </div>  
+        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 p-2 mr-auto ml-2">
+        <h1 className="text-uppercase">{attributes.title}</h1>
+          
+          <p style={introStyle}>
+            {attributes.introduction}
+            <a href={attributes.link_url_01} >{attributes.link_name_01}</a>
+          </p>
+        </div>
+       
+      
+      <style jsx>
+      {`
+        img
+        {
+          maxWidth:320px;
+          
+          display:cover;
+
+        }
+      `}
+      
+      </style>
       </div>
-      <p style={introStyle}>
-        {attributes.introduction}
-        <a href={attributes.link_url_01} >{attributes.link_name_01}</a>
-      </p>
+
     </div>
   </Layout>
 );
