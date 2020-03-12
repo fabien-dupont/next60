9ad2ec442708ae77cee485ef7d2c2b21abce3f42
Fabien Dupont <fab.d1987@yandex.com>
2020-03-12 16:51:45 +0100

    Create Navlist.js

---
+++ components/header/Navlist.js
@@ -0,0 +1,19 @@
+import Link from "next/link" ;
+
+const Navlist = (props) => {
+  return (
+       <ul className="nav navbar-nav flex-row">
+        <li className="m-auto p-2">
+          <Link href="/"><a>Home</a></Link>
+        </li>
+        <li className="m-auto p-2">
+          <Link href="about"><a>About</a></Link>
+        </li>
+        <li className="m-auto p-2">
+          <Link href="contact"><a>Contact</a></Link>
+        </li>
+      </ul>
+  );
+};
+
+export default Navlist;
