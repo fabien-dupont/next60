c83043b3cf80426c1e7b78967db9f4dd1b83a911
Fabien Dupont <fab.d1987@yandex.com>
2020-03-12 16:51:12 +0100

    insert Link

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,8 @@
-import Link from "next/link" ;
+import Link from "next/link";
 const Footer = () => (
   <footer className="footer pt-4 mt-4  text-center text-md-left">
     <div className="container-fluid">
-        © 2020 - <a href="https://github.com/fabien-dupont">Fabien Dupont</a> 
+        © 2020 - <Link href="https://github.com/fabien-dupont"><a>Fabien Dupont</a> </Link>
      </div>
   </footer>
 );
