655e82b678cccc0a84d575d77cc4cbfbda441126
Fabien Dupont <fab.d1987@yandex.com>
2020-03-14 12:18:43 +0100

    64px margin top

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,12 @@ import { attributes, html } from '../content/index.md'
 
 const imgURL = "https://source.unsplash.com/random";
 const imgStyle = {display: "contain",position:"relative" };
-const sectionStyle = {};
+const sectionStyle = {marginTop:64};
 const paragraphStyle = {fontSize: 24,lineHeight:2};
 
 const Index = () => (<>
   <Header/>
-  <section className="section w-100 h-100 m-0 p-0" style={sectionStyle}>
+  <section className="section w-100 h-100 p-0" style={sectionStyle}>
     <div className="container-fluid m-0 p-0" >
       <div className = "row m-0 p-0">
         <div id="zone_api_result"  className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-2 m-0 p-0">
