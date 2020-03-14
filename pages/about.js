062ec64f96d6768efa65c19ea8ca050c67a74bf8
Fabien Dupont <fab.d1987@yandex.com>
2020-03-14 12:56:05 +0100

    common structure

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,22 @@
  
+import Header from '../components/Header'
 import { attributes, html } from '../content/about.md'
-const introductionStyle = {fontSize: 24};
+const sectionStyle = {marginTop:64};
+const paragraphStyle = {fontSize: 24,lineHeight:2};
 
-const About = () => (
- 
-    <div>
-      <h1>{attributes.title}</h1>
-      <p style={introductionStyle}>
-        {attributes.introduction}
-      </p>
+const About = () => (<>
+  <Header/>
+  <section className="section w-100 h-100 p-0" style={sectionStyle}>
+    <div className="container-fluid m-0 p-0" >
+      <div className = "row m-0 p-0">
+        <div className="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-9 m-0 p-4">
+          <h1>{attributes.title}</h1>
+          <p style={paragraphStyle}>
+          {attributes.introduction}
+          </p>
+        </div>
+      </div>
     </div>
- 
-);
+  </section>
+</>);
 export default About;
