a71b28315189d4907f4f51b4ae8c5582fd3beaf8
Fabien Dupont <fab.d1987@yandex.com>
2020-03-14 12:09:41 +0100

    change introStyle to introductionStyle

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,12 @@
  
 import { attributes, html } from '../content/about.md'
-const introStyle = {fontSize: 24};
+const introductionStyle = {fontSize: 24};
 
 const About = () => (
  
     <div>
       <h1>{attributes.title}</h1>
-      <p style={introStyle}>
+      <p style={introductionStyle}>
         {attributes.introduction}
       </p>
     </div>
--- pages/contact.js
+++ pages/contact.js
@@ -1,12 +1,12 @@
  
 import { attributes, html } from '../content/contact.md'
-const introStyle = {fontSize: 24};
+const introductionStyle = {fontSize: 24};
 
 const Contact = () => (
  
     <div> 
       <h1>{attributes.title}</h1>
-      <p style={introStyle}>
+      <p style={introductionStyle}>
         {attributes.introduction}
       </p>
     </div>
