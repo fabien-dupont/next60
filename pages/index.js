33eb8e88f90dbb8fa0297dbcddae8840211ae3ac
Fabien Dupont <fab.d1987@yandex.com>
2020-03-14 12:58:14 +0100

    common structure

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,10 @@ const Index = () => (<>
   <section className="section w-100 h-100 p-0" style={sectionStyle}>
     <div className="container-fluid m-0 p-0" >
       <div className = "row m-0 p-0">
-        <div id="zone_api_result"  className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-2 m-0 p-0">
+        <div id="zone_api_result"  className="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 m-0 p-0">
           <img className="p-0 m-0" src={imgURL} width={"100%"} style={imgStyle}/> 
         </div>        
-        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-8 col-xl-10 m-0 p-4">
+        <div className="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-9 m-0 p-4">
           <h1 className="text-uppercase">{attributes.title}</h1>
           <p className="text-uppercase text-strong" style={paragraphStyle}>
             {attributes.introduction}
