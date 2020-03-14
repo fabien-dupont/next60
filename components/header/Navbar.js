f61f658798d46b87059a322632fdcba090e451b3
Fabien Dupont <fab.d1987@yandex.com>
2020-03-14 12:56:53 +0100

    clean up

--- components/header/Navbar.js
+++ components/header/Navbar.js
@@ -1,24 +1,8 @@
-import Link from "next/link" ;
-import Navheader from "./Navheader" ;
-const Navbar = () =>   (
+const Navbar = (props) => (
   <nav className="navbar navbar-expand-lg fixed-top bg-white">
     <div className="container-fluid">
-      <Navheader/>
-      
-      <ul className="nav navbar-nav flex-row">
-        <li className="m-auto p-2">
-          <Link href="/"><a>Home</a></Link>
-        </li>
-        <li className="m-auto p-2">
-          <Link href="about"><a>About</a></Link>
-        </li>
-        <li className="m-auto p-2">
-          <Link href="contact"><a>Contact</a></Link>
-        </li>
-      </ul>
+      {props.children}
     </div>
   </nav>
   );
- 
-
 export default Navbar;
