ec3580b55f80492c18332c16db9d1ef5472efd96
Fabien Dupont <fab.d1987@yandex.com>
2020-03-16 15:34:41 +0100

    Create Sidebar.js

---
+++ components/Sidebar.js
@@ -0,0 +1,12 @@
+
+const SidebarStyle = {backgroundColor:"#fff",position:"fixed",left:0,top:0,overflowX:"hidden",height:"100%",width:160,zIndex:129};
+
+const Sidebar = () => (<>
+<aside style={SidebarStyle}>
+  <div className="container">
+    This is the sidebar
+  </div>
+</aside>
+
+</>); 
+export default Sidebar ;
