fa7ae348c40da3305c1f9dc377804b068721a733
Fabien Dupont <fab.d1987@yandex.com>
2020-03-16 15:34:33 +0100

    Update SplashDiv.js

--- pages/homepage_Components/SplashDiv.js
+++ pages/homepage_Components/SplashDiv.js
@@ -1,6 +1,6 @@
 const imgURL = "https://source.unsplash.com/random";
-const imgStyle = {display: "cover",position:"relative",minHeight:480,width:"100%"};
-const splashDivContainer = {zIndex:10, minHeight:"100%"}
+const imgStyle = {display: "cover",bottom:0,minWidth:360,width:"100%"};
+const splashDivContainer = {position:"absolute",zIndex:10, minHeight:"100%"}
 const SplashDiv = () => (<>
   <div id="SplashDiv"  className="w-100 m-0 p-0" style={splashDivContainer}>
     <img className="p-0 m-0" src={imgURL}  style={imgStyle}/> 
