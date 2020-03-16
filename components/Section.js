675bbdc176d2ff2d86414177f6541f14dbe76efb
Fabien Dupont <fab.d1987@yandex.com>
2020-03-16 14:22:09 +0100

    Create Section.js

---
+++ components/Section.js
@@ -0,0 +1,11 @@
+const sectionStyle = {marginTop:64};
+
+const Section = (props) => (<>
+  <section className="section w-100 h-100 p-0" style={sectionStyle}>
+    <div className="container-fluid m-0 p-0" >
+      <div className = "row m-0 p-0">
+        {props.children}
+      </div>
+    </div>        
+  </section>
+</>); export default Section;
