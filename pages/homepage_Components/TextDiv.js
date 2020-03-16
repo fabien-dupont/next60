14538d2cd69d8bbdd0133b559f5b277635b5e9f5
Fabien Dupont <fab.d1987@yandex.com>
2020-03-16 20:30:23 +0100

    Update TextDiv.js

--- pages/homepage_Components/TextDiv.js
+++ pages/homepage_Components/TextDiv.js
@@ -1,4 +1,4 @@
-const Div_Style= {overflowY:"hidden",zIndex:11,position:"absolute",bottom:64,backgroundColor:"rgba(255,255,255,0.9)" };
+const Div_Style= {overflowY:"hidden",zIndex:11,position:"absolute",top: 256,backgroundColor:"rgba(255,255,255,0.9)" };
 const TextDiv= (props) =>(<>
   <div style={Div_Style} className="100-w p-4" id="_Div01">
     {props.children}
