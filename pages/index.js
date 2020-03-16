c64f095d5a49c0b484d521e0e26b33485df28e32
Fabien Dupont <fab.d1987@yandex.com>
2020-03-16 20:30:35 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,21 @@
 import Header from '../components/Header'
 import Section from '../components/Section'
-import SplashDiv from "./homepage_Components/SplashDiv"
+import UnsplashDiv from "./homepage_Components/UnsplashDiv"
 import TextDiv from './homepage_Components/TextDiv'
 import { attributes, html } from '../content/index.md' 
 
-const paragraphStyle = {fontSize: 24,lineHeight:2}; 
+const paragraphStyle = {fontSize:24,lineHeight:2}; 
 const Index = () => (<>
   <Header/>
   <Section>
-    <SplashDiv/> 
-    
+    <UnsplashDiv/> 
     <TextDiv>
       <h1 className="text-uppercase">{attributes.title}</h1>
       <p className="text-uppercase text-strong" style={paragraphStyle}>
-        {attributes.introduction}
-        <a href={attributes.link_url_01} >{attributes.link_name_01}</a>
+        {attributes.introduction} - 
+        <a href={attributes.link_url_01}>
+          {attributes.link_name_01}
+        </a>
       </p>
     </TextDiv>
   </Section>
